0706c1e1791214f6bf6d2710a2dfc2ce43e2a8c2
sumitshinde2608 <shindesumit268@gmail.com>
2021-09-29 17:58:10 +0530

    minor change

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const cors = require('cors');
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const dbConfig = require("./app/config/db.config");
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
-const router = require('./router');
+const router = require("./router");
 
 const app = express();
 const server = http.createServer(app);
@@ -24,21 +24,21 @@ Role = db.role;
 Chat = db.chat;
 
 db.mongoose
-  .connect(dbConfig.URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
+	.connect(dbConfig.URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Successfully connect to MongoDB.");
+	})
+	.catch((err) => {
+		console.error("Connection error", err);
+		process.exit();
+	});
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Clarify Me test application" });
+	res.json({ message: "Welcome to Clarify Me test application" });
 });
 
 // routes
@@ -46,40 +46,54 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/slots.routes")(app);
 
-io.on('connect', (socket) => {
-  socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+io.on("connect", (socket) => {
+	socket.on("join", ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) return callback(error);
+		if (error) return callback(error);
 
-    socket.join(user.room);
+		socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+		socket.emit("message", {
+			user: "admin",
+			text: `${user.name}, welcome to room ${user.room}.`,
+		});
+		socket.broadcast
+			.to(user.room)
+			.emit("message", { user: "admin", text: `${user.name} has joined!` });
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
 
-    callback();
-  });
+		callback();
+	});
 
-  socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
+	socket.on("sendMessage", (message, callback) => {
+		const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message });
+		io.to(user.room).emit("message", { user: user.name, text: message });
 
-    let chatMessage = new Chat({ message: message, sender: user.name });
-    chatMessage.save();
-    callback();
-  });
+		let chatMessage = new Chat({ message: message, sender: user.name });
+		chatMessage.save();
+		callback();
+	});
 
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
+	socket.on("disconnect", () => {
+		const user = removeUser(socket.id);
 
-    if (user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-    }
-  })
+		if (user) {
+			io.to(user.room).emit("message", {
+				user: "Admin",
+				text: `${user.name} has left.`,
+			});
+			io.to(user.room).emit("roomData", {
+				room: user.room,
+				users: getUsersInRoom(user.room),
+			});
+		}
+	});
 });
 
-server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+server.listen(5000, () => console.log(`Server has started at port 5000.`));
